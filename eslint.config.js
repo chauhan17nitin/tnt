6f7cd25e3637e47567a13aa188344756353b4485
Abhishek Soni <abhisheksoni2720@gmail.com>
2025-05-28 20:03:20 +0530

    feat: add linter

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        chrome: 'readonly',
+        setInterval: 'readonly',
+        setTimeout: 'readonly',
+        clearInterval: 'readonly',
+        clearTimeout: 'readonly',
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
